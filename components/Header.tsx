6114cd03dad3278deeb4f4223839ab656b091ea6
Jimmy Hannan <jim9112@gmail.com>
2021-09-09 13:30:44 -0400

    header style changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,18 @@ import Image from 'next/image';
 import hamburger from '../public/shared/mobile/icon-hamburger.svg';
 const Header = () => {
   return (
-    <header className='bg-header-mobile sm:bg-header-tablet md:bg-header-desktop bg-no-repeat'>
-      <div>
-        <h2>Frontend Mentor</h2>
-        <h3>Feedback Board</h3>
-      </div>
-      <div>
-        <Image src={hamburger} alt='hamburger' />
+    <header>
+      <div className='bg-header-mobile sm:bg-header-tablet md:bg-header-desktop bg-no-repeat grid grid-flow-col px-7 py-4'>
+        <div className='text-white'>
+          <h2>Frontend Mentor</h2>
+          <h3>Feedback Board</h3>
+        </div>
+
+        <div className='sm:hidden grid justify-end items-center'>
+          <div>
+            <Image src={hamburger} alt='hamburger' />
+          </div>
+        </div>
       </div>
     </header>
   );
