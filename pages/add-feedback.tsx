ee39f925eb20e6184e8b1dc6986b1b026e7a9bd8
Jimmy Hannan <jim9112@gmail.com>
2021-09-23 13:04:23 -0400

    style form

--- pages/add-feedback.tsx
+++ pages/add-feedback.tsx
@@ -1,16 +1,33 @@
+import Link from 'next/link';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
 const addFeedback = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
-    <div className='bg-text-grey min-h-screen'>
-      <div className='bg-text-white'>
-        <h1>Create New Feedback</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label> Feedback Title</label>
+    <div className='bg-text-grey min-h-screen px-6'>
+      <div>
+        <Link href='/'>Go Back</Link>
+      </div>
+      <div className='bg-text-white p-6'>
+        <h1 className='font-bold text-text-secondary text-lg'>
+          Create New Feedback
+        </h1>
+        <form className='grid grid-flow-row' onSubmit={handleSubmit(onSubmit)}>
+          <label className='text-sm text-text-secondary font-bold'>
+            {' '}
+            Feedback Title
+          </label>
+          <p className='text-text-secondary-light text-sm'>
+            Add a short, descriptive headline
+          </p>
           <input className='bg-text-grey' {...register('feedbackTitle')} />
-          <label>Category</label>
+          <label className='text-sm text-text-secondary font-bold'>
+            Category
+          </label>
+          <p className='text-text-secondary-light text-sm'>
+            Choose a category for your feedback
+          </p>
           <select {...register('category')}>
             <option value='UI'>UI</option>
             <option value='UX'>UX</option>
@@ -18,8 +35,15 @@ const addFeedback = () => {
             <option value='Bug'>Bug</option>
             <option value='Feature'>Feature</option>
           </select>
-          <label>Feedback Detail</label>
+          <label className='text-sm text-text-secondary font-bold'>
+            Feedback Detail
+          </label>
+          <p className='text-text-secondary-light text-sm'>
+            Include any specific comments on what should be improved, added,
+            etc.
+          </p>
           <textarea
+            className='bg-text-grey'
             {...register('feedbackDetail')}
             cols={30}
             rows={10}
