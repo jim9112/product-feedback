bb4ab69f424d4a1336c296a541813ca3a9afde6f
Jimmy Hannan <jim9112@gmail.com>
2021-09-27 12:32:39 -0400

    add comment content

--- components/SingleComment.tsx
+++ components/SingleComment.tsx
@@ -1,11 +1,32 @@
+import Image from 'next/image';
+
 interface IProps {
-  comment: {};
+  comment: {
+    content: string;
+    user: {
+      image: string;
+      name: string;
+      username: string;
+    };
+  };
 }
 
 const SingleComment = ({ comment }: IProps) => {
   return (
     <div>
-      <h1>Single Comment</h1>
+      <div className='flex relative gap-4'>
+        <div className='w-10 h-10 relative rounded-full overflow-hidden'>
+          <Image src={comment.user.image} alt='users picture' layout='fill' />
+        </div>
+        <div>
+          <p>{comment.user.name}</p>
+          <p>@{comment.user.username}</p>
+        </div>
+        <div className='self-center absolute right-0'>Reply</div>
+      </div>
+      <div>
+        <p>{comment.content}</p>
+      </div>
     </div>
   );
 };
