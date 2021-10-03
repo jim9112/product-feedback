a44454b5eae87c85989d0c6ed2e068e615997708
Jimmy Hannan <jim9112@gmail.com>
2021-10-03 12:33:54 -0400

    add container to toolbar

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -2,28 +2,30 @@ import Button from './Button';
 
 const Toolbar = () => {
   return (
-    <div className='bg-bg-dark px-6 py-2 grid grid-flow-col items-center'>
-      <label className='text-text-grey text-sm' htmlFor='sortOptions'>
-        Sort by:
-        <select
-          className='bg-bg-dark text-text-white font-bold'
-          name='sortOptions'
-          id='sortOptions'
-        >
-          <option value='mostUpvotes'>Most Upvotes</option>
-          <option value='leastUpvotes'>Least Upvotes</option>
-          <option value='mostComments'>Most Comments</option>
-          <option value='leastComments'>Least Comments</option>
-        </select>
-      </label>
-      <Button
-        type='button'
-        content='+ Add Feedback'
-        color='primary'
-        cursor='pointer'
-        size='small'
-        clickAction='addFeedback'
-      />
+    <div className='sm:px-10'>
+      <div className='bg-bg-dark px-6 py-2 grid grid-flow-col items-center sm:rounded-lg'>
+        <label className='text-text-grey text-sm' htmlFor='sortOptions'>
+          Sort by:
+          <select
+            className='bg-bg-dark text-text-white font-bold'
+            name='sortOptions'
+            id='sortOptions'
+          >
+            <option value='mostUpvotes'>Most Upvotes</option>
+            <option value='leastUpvotes'>Least Upvotes</option>
+            <option value='mostComments'>Most Comments</option>
+            <option value='leastComments'>Least Comments</option>
+          </select>
+        </label>
+        <Button
+          type='button'
+          content='+ Add Feedback'
+          color='primary'
+          cursor='pointer'
+          size='small'
+          clickAction='addFeedback'
+        />
+      </div>
     </div>
   );
 };
