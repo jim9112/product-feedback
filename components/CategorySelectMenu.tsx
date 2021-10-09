b2754feab1ef777c632af76cb8cd0191a1ddc1c7
Jimmy Hannan <jim9112@gmail.com>
2021-10-09 17:26:04 -0400

    add event handler to tag menu

--- components/CategorySelectMenu.tsx
+++ components/CategorySelectMenu.tsx
@@ -1,9 +1,22 @@
+import React from 'react';
 import categorys from '../lib/content/categorys.json';
 import Tag from './Tag';
 
 const CategorySelectMenu = () => {
+  const clickHandler = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLElement;
+    if (target.id !== 'tagContainer') {
+      console.log(target.textContent);
+      console.log(target);
+    }
+  };
   return (
-    <div className='bg-text-white rounded-lg py-6 pl-6 flex flex-wrap gap-x-2 gap-y-4 sm:h-full'>
+    <div
+      id='tagContainer'
+      className='bg-text-white rounded-lg py-6 pl-6 flex flex-wrap gap-x-2 gap-y-4 sm:h-full'
+      onClick={clickHandler}
+    >
+      {/* returns a tag component for every tag listed in the categorys.json file */}
       {categorys.tags.map((tag, i) => (
         <Tag key={i} category={tag} />
       ))}
