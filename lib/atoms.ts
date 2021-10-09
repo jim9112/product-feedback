9882d34ea9001a3fa7ed16e2ff0288de318b595a
Jimmy Hannan <jim9112@gmail.com>
2021-10-09 17:32:04 -0400

    add an atom for tag filter

--- lib/atoms.ts
+++ lib/atoms.ts
@@ -1,12 +1,20 @@
 import { atom } from 'recoil';
 import data from './content/data.json';
 
+// state for all of the product requests
 export const productRequestState = atom({
   key: 'productRequestState',
   default: data.productRequests,
 });
 
+// state that holds any filters being applied to the product request display
 export const productRequestFilterState = atom({
   key: 'productRequestFilterState',
   default: 'Most Upvotes',
+});
+
+// state that holds any tag filters being applied to the product request display
+export const tagFilterState = atom({
+  key: 'tagFilterState',
+  default: 'all',
 });
