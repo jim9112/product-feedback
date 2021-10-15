77df1e4cff9d62366a707ef0d9bd02b9e5e2555e
Jimmy Hannan <jim9112@gmail.com>
2021-10-15 11:50:06 -0400

    add selectability to roadmap in mobile veiw

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -1,10 +1,16 @@
 import BackButton from '../components/global/BackButton';
 import useSortRoadmapRequests from '../lib/hooks/useSortRoadmapRequests';
 import RoadmapRequestCard from '../components/roadmap-page/RoadmapRequestCard';
+import { useState } from 'react';
 
 const Roadmap = () => {
   // get lists of requests sorted by status
   const { roadmapState } = useSortRoadmapRequests();
+
+  // state that stores which category to display on mobile
+  const [displayCategoryState, setDisplayCategoryState] = useState<
+    'planned' | 'live' | 'progress'
+  >('progress');
 
   return (
     <div className='bg-text-grey min-h-screen lg:px-44 lg:py-20'>
@@ -14,29 +20,59 @@ const Roadmap = () => {
       </header>
       <div className='text-text-secondary grid grid-cols-3'>
         <div>
-          <h2>Planned</h2>
+          <h2
+            className='cursor-pointer md:pointer-events-none'
+            onClick={() => setDisplayCategoryState('planned')}
+          >
+            Planned ({roadmapState?.planned.length})
+          </h2>
+          <p className='hidden md:inline'>Ideas prioritized for research</p>
         </div>
         <div>
-          <h2>In-Progress</h2>
+          <h2
+            className='cursor-pointer md:pointer-events-none'
+            onClick={() => setDisplayCategoryState('progress')}
+          >
+            In-Progress ({roadmapState?.progress.length})
+          </h2>
+          <p className='hidden md:inline'>Currently being developed</p>
         </div>
         <div>
-          <h2>Live</h2>
+          <h2
+            className='cursor-pointer md:pointer-events-none'
+            onClick={() => setDisplayCategoryState('live')}
+          >
+            Live ({roadmapState?.live.length})
+          </h2>
+          <p className='hidden md:inline'>Released features</p>
         </div>
       </div>
-      <div className='text-text-secondary grid grid-cols-3'>
-        <div>
+      <div className='text-text-secondary grid grid-cols-1 md:grid-cols-3'>
+        <div
+          className={`${
+            displayCategoryState === 'planned' ? 'block' : 'hidden'
+          } md:block`}
+        >
           {roadmapState?.planned &&
             roadmapState?.planned.map((request) => (
               <RoadmapRequestCard key={request.id} request={request} />
             ))}
         </div>
-        <div>
+        <div
+          className={`${
+            displayCategoryState === 'progress' ? 'block' : 'hidden'
+          } md:block`}
+        >
           {roadmapState?.progress &&
             roadmapState?.progress.map((request) => (
               <RoadmapRequestCard key={request.id} request={request} />
             ))}
         </div>
-        <div>
+        <div
+          className={`${
+            displayCategoryState === 'live' ? 'block' : 'hidden'
+          } md:block`}
+        >
           {roadmapState?.live &&
             roadmapState?.live.map((request) => (
               <RoadmapRequestCard key={request.id} request={request} />
