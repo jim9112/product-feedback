7ea2423186e135cac5bd68a6e53f801e6b0a1a38
Jimmy Hannan <jim9112@gmail.com>
2021-11-01 12:00:30 -0400

    add new comment to state

--- components/forms/AddCommentForm.tsx
+++ components/forms/AddCommentForm.tsx
@@ -1,18 +1,57 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Button from '../global/Button';
+import { IFeedback } from '../../lib/typesInterface';
+import { currentUserState, productRequestState } from '../../lib/atoms';
+import { useRecoilState, useRecoilValue } from 'recoil';
+
+interface IProps {
+  feedback: IFeedback;
+}
 
 interface IFormInput {
   comment: string;
 }
 
-const AddCommentForm = () => {
+const AddCommentForm = ({ feedback }: IProps) => {
   const { register, handleSubmit, watch } = useForm();
+  const currentUser = useRecoilValue(currentUserState);
+  const [feedbackState, setFeedbackState] = useRecoilState(productRequestState);
 
   //   To Do: save form data to state
-  const onSubmit = (data: IFormInput) => console.log(data);
+  const onSubmit = (data: IFormInput) => {
+    // temporary object with currently selected feedback
+    const tempObj = { ...feedback };
+
+    // create new comment object with form data
+    const newComment = {
+      id: Date.now(),
+      content: data.comment,
+      user: currentUser,
+    };
+
+    // iff a comment array exisists update with new comment else create new array with comment
+    if (feedback.comments) {
+      tempObj.comments = [...feedback.comments, newComment];
+    } else {
+      tempObj.comments = [newComment];
+    }
+
+    // map feedback to a temporary array while updating comment section of current feedback
+    const tempAllFeedback = feedbackState.map((el) => {
+      if (el.id === feedback.id) {
+        return tempObj;
+      } else {
+        return el;
+      }
+    });
+
+    // update state
+    setFeedbackState(tempAllFeedback);
+  };
 
   //   watch the comment field of form to get amount of characters used
   let commentData = watch('comment', '');
+
   return (
     <div className='bg-text-white p-6 rounded-xl'>
       <h1 className='text-lg text-text-secondary font-bold mb-6'>
