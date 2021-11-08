80da25f570de72e403309db81b7338a899f63ca0
Jimmy Hannan <jim9112@gmail.com>
2021-11-08 11:57:35 -0500

    clean up custom hook

--- lib/hooks/useAddComment.ts
+++ lib/hooks/useAddComment.ts
@@ -2,16 +2,10 @@ import { Dispatch, SetStateAction, useEffect } from 'react';
 import { FormState } from 'react-hook-form';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { currentUserState, productRequestState } from '../atoms';
-import { IFeedback, IComment } from '../typesInterface';
+import { IFeedback } from '../typesInterface';
 
 interface IFormInput {
   comment: string;
-}
-
-interface IReplies {
-  content: string;
-  replyingTo: string;
-  user: { image: string; name: string; username: string };
 }
 
 const useAddComment = (
@@ -31,8 +25,31 @@ const useAddComment = (
   // temporary object with currently selected feedback
   const tempObj = { ...feedback };
 
+  const addToStateAndClose = () => {
+    // map feedback to a temporary array while updating comment section of current feedback
+    const tempAllFeedback = feedbackState.map((el) => {
+      if (el.id === feedback.id) {
+        return tempObj;
+      } else {
+        return el;
+      }
+    });
+
+    // update state
+    setFeedbackState(tempAllFeedback);
+
+    // close reply form if replying to comment
+    if (setFormDisplay) setFormDisplay(false);
+  };
+
   //  Called when someone comments on a feature request
-  const addComment = (newComment: IComment) => {
+  const addComment = (data: IFormInput) => {
+    // create new comment object with form data
+    const newComment = {
+      id: Date.now(),
+      content: data.comment,
+      user: currentUser,
+    };
     // if a comment array exisists update with new comment else create new array with comment
     if (feedback.comments) {
       tempObj.comments = [...feedback.comments, newComment];
@@ -40,74 +57,52 @@ const useAddComment = (
       tempObj.comments = [newComment];
     }
 
-    // map feedback to a temporary array while updating comment section of current feedback
-    const tempAllFeedback = feedbackState.map((el) => {
-      if (el.id === feedback.id) {
-        return tempObj;
-      } else {
-        return el;
-      }
-    });
-
-    // update state
-    setFeedbackState(tempAllFeedback);
+    // add to state and close form
+    addToStateAndClose();
   };
 
   // called when someone replies to a comment
-  const addReply = (newReply: IReplies) => {
-    // build a new comment object that inclueds the replies
-    const tempComments = tempObj.comments?.map((element) => {
-      if (element.id === commentId && element.replies) {
-        newReply.replyingTo === element.user.username;
-        const newElement = { ...element };
-        newElement.replies = [...element.replies, newReply];
-        return newElement;
-      } else if (element.id === commentId && !element.replies) {
-        newReply.replyingTo === element.user.username;
-        const newElement = { ...element };
-        newElement.replies = [newReply];
-        return newElement;
-      } else {
-        return element;
-      }
-    });
-
-    tempObj.comments = tempComments;
-
-    // map feedback to a temporary array while updating comment section of current feedback
-    const tempAllFeedback = feedbackState.map((el) => {
-      if (el.id === feedback.id) {
-        return tempObj;
-      } else {
-        return el;
-      }
-    });
-
-    // update state
-    setFeedbackState(tempAllFeedback);
-
-    // close reply form
-    if (setFormDisplay) setFormDisplay(false);
-  };
-
-  // submit function called from form
-  const onSubmit = (data: IFormInput) => {
+  const addReply = (data: IFormInput) => {
     // create new comment object with form data
-    const newComment = {
-      id: Date.now(),
-      content: data.comment,
-      user: currentUser,
-    };
     const newReply = {
       id: Date.now(),
       content: data.comment,
       replyingTo: '',
       user: currentUser,
     };
+    // build a new comment object that inclueds the replies
+    const tempComments = tempObj.comments?.map((element) => {
+      // if comment id matches id cof comment being replied and a reply array exisits update it
+      if (element.id === commentId && element.replies) {
+        newReply.replyingTo === element.user.username;
+        const newElement = { ...element };
+        newElement.replies = [...element.replies, newReply];
+        return newElement;
+        // if ID matches and no reply array exsists create one and ad reply to it
+      } else if (element.id === commentId && !element.replies) {
+        newReply.replyingTo === element.user.username;
+        const newElement = { ...element };
+        newElement.replies = [newReply];
+        return newElement;
+        // else return origional comment object
+      } else {
+        return element;
+      }
+    });
+
+    // add new comments array to temp object
+    tempObj.comments = tempComments;
+
+    // add to state and close form
+    addToStateAndClose();
+  };
+
+  // submit function called from form
+  const onSubmit = (data: IFormInput) => {
     if (commentType === 'comment') {
-      addComment(newComment);
+      addComment(data);
     } else if (commentType === 'reply') {
-      addReply(newReply);
+      addReply(data);
     }
   };
 
